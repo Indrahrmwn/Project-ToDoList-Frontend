f74e53e5be0702e8b70a1b5df5e019bbcf9e5806
indra <indramereun015@gmail.com>
2025-07-23 11:05:05 +0700

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,18 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-function Navbar() {
-  return (
-    <nav className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-8 py-6 bg-white shadow-[0_4px_12px_rgba(0,0,0,0.1)]">
-      <h1 className="text-4xl font-bold text-green-700">ToDoin</h1>
-      <div className="flex justify-center">
-        <Link
-          to="/login"
-          className="bg-green-600 text-white text-xl md:text-2xl font-semibold px-8 py-4 rounded-lg hover:bg-green-700 transition">
-          Create Your List
-        </Link>
-      </div>
-    </nav>
-  );
-}
+// function Navbar() {
+//   return (
+//     <nav className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-8 py-6 bg-white shadow-[0_4px_12px_rgba(0,0,0,0.1)]">
+//       <h1 className="text-4xl font-bold text-green-700">ToDoin</h1>
+//       <div className="flex justify-center">
+//         <Link
+//           to="/login"
+//           className="bg-green-600 text-white text-xl md:text-2xl font-semibold px-8 py-4 rounded-lg hover:bg-green-700 transition">
+//           Create Your List
+//         </Link>
+//       </div>
+//     </nav>
+//   );
+// }
 
-export default Navbar;
+// export default Navbar;
